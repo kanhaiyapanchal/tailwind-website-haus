d746bdf6d89c138cf6a8e27dc4611be4cc96a9f2
kanhaiyapanchal <kanhaiyapanchal03@gmail.com>
2023-03-01 18:59:42 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'p1-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/src/public/image/p1.jpg')",
-        'p2-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/src/public/image/p2.jpg')",
-        'p3-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/src/public/image/p3.jpg')",
-        'p4-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/src/public/image/p4.jpg')",
+        'p1-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/public/image/p1.jpg')",
+        'p2-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/public/image/p2.jpg')",
+        'p3-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/public/image/p3.jpg')",
+        'p4-image': "url('/FT_2.3_Tailwind/Day2/Assignment1/Source/tailwind3-master/public/image/p4.jpg')",
 
       }
     },
